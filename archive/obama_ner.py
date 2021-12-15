85570e9bd65aba0a8e9a9b4ed8de1d9a35cb3769
Yujian Tang <ytang07@gmail.com>
2021-12-14 17:01:34 -0800

    adding sentence polarities and completing ner

--- archive/obama_ner.py
+++ archive/obama_ner.py
@@ -12,24 +12,78 @@ headers = {
 }
 ner_url = _url + "ner"
 
-# loop through each year
-for i in list(range(2008, 2018)):
-    with open(f"obama_{i}.txt", "r") as f:
-        headlines = f.readlines()
-    # combine list of headlines into one text
-    text = "".join(headlines)
-    
-    # set up request bodies
-    body = {
-        "text": text,
-        "labels": "ARTICLE"
-    }
-    # parse responses
-    response = requests.post(url=ner_url, headers=headers, json=body)
-    _dict = json.loads(response.text)
-    with open(f"obama/{i}_ner.txt", "w") as f:
-        for entry in _dict["ner"]:
-            ner = entry[0] + ": " + entry[1]
-            f.write(ner)
-            f.write('\n')
-    # save results to a txt
+def get_ners():
+    # loop through each year
+    for i in list(range(2008, 2018)):
+        with open(f"obama_{i}.txt", "r") as f:
+            headlines = f.readlines()
+        # combine list of headlines into one text
+        text = "".join(headlines)
+        
+        # set up request bodies
+        body = {
+            "text": text,
+            "labels": "ARTICLE"
+        }
+        # parse responses
+        response = requests.post(url=ner_url, headers=headers, json=body)
+        _dict = json.loads(response.text)
+        # save results to a txt
+        with open(f"obama/{i}_ner.txt", "w") as f:
+            for entry in _dict["ner"]:
+                ner = entry[0] + ": " + entry[1]
+                f.write(ner)
+                f.write('\n')
+
+# get_ners()
+
+# find number of mentions of each type of entity
+# dictionary of type: dictionary of name: count
+def analyze_ner(year):
+    with open(f"obama/{year}_ner.txt", "r") as f:
+        ners = f.readlines()
+    # type to dictionary dict
+    outer_dict = {}
+    for ner in ners:
+        elements = ner.split(":")
+        try:
+            _type = elements[0]
+            _name = elements[1][:-1]
+            if "'s" in _name:
+                _name = _name.replace("'s", "")
+            if _name[0] == " ":
+                _name = _name[1:]
+        except:
+            continue
+        # find number of mentions of each entity within types
+        if _type in outer_dict:
+            if _name in outer_dict[_type]:
+                outer_dict[_type][_name] += 1
+            else:
+                outer_dict[_type][_name] = 1
+        else:
+            inner_dict = {}
+            inner_dict[_name] = 1
+            outer_dict[_type] = inner_dict
+    with open(f"obama/{year}_analyzed_ner.json", "w") as f:
+        json.dump(outer_dict, f)
+
+# for i in range(2008, 2018):
+#     analyze_ner(i)
+
+# find most commonly mentioned entity of each type
+def most_common(year):
+    with open(f"obama/{year}_analyzed_ner.json", "r") as f:
+        entries = json.load(f)
+    for _type in entries:
+        _sorted = sorted(entries[_type].items(), key = lambda item: item[1], reverse=True)
+        if _type == 'PERSON':
+            print(f"Most common {_type} (other than Obama) in headlines about Obama in {year} was {_sorted[1][0]} mentioned {_sorted[1][1]} times")
+        else:
+            print(f"Most common {_type} in headlines about Obama in {year} was {_sorted[0][0]} mentioned {_sorted[0][1]} times")
+    print('\n')
+
+for i in range(2008, 2018):
+    most_common(i)
+
+# word cloud each one
--- archive/obama_polarity_by_sentence.py
+++ archive/obama_polarity_by_sentence.py
@@ -1,6 +1,7 @@
 # import libraries
 import requests
 import json
+import matplotlib.pyplot as plt
 import sys
 sys.path.append("../..")
 from nyt.config import thetextapikey, _url
@@ -13,25 +14,52 @@ headers = {
 polarity_by_sentence_url = _url + "polarity_by_sentence"
 
 # loop through each year
-for i in list(range(2008, 2018)):
-    with open(f"obama_{i}.txt", "r") as f:
-        headlines = f.readlines()
-    # combine list of headlines into one text
-    text = "".join(headlines)
-    text.replace('\n', "")
-    
-    # set up request bodies
-    body = {
-        "text": text
-    }
-    # parse responses
-    response = requests.post(url=polarity_by_sentence_url, headers=headers, json=body)
-    _dict = json.loads(response.text)
-    # save to text file
-    with open(f"obama/{i}_sentence_polarities.txt", "w") as f:
-        for entry in _dict["polarity by sentence"]:
-            f.write(f"{entry[1]} : {entry[0]}")
-    
-# plot each datapoint
+def get_polarities():
+    for i in list(range(2008, 2018)):
+        with open(f"obama_{i}.txt", "r") as f:
+            headlines = f.readlines()
+        # combine list of headlines into one text
+        text = "".join(headlines)
+        text.replace('\n', "")
+        
+        # set up request bodies
+        body = {
+            "text": text
+        }
+        # parse responses
+        response = requests.post(url=polarity_by_sentence_url, headers=headers, json=body)
+        _dict = json.loads(response.text)
+        # save to text file
+        with open(f"obama/{i}_sentence_polarities.txt", "w") as f:
+            for entry in _dict["polarity by sentence"]:
+                f.write(f"{entry[1]} : {entry[0]}")
 
+# get_polarities()
+
+# plot each datapoint
+def plot_polarities(year):
+    with open(f"obama/{year}_sentence_polarities.txt", "r") as f:
+        entries = f.readlines()
+    ys = []
+    titles = []
+    for entry in entries:
+        try:
+            _entry = entry.split(": ")
+            ys.append(float(_entry[1]))
+            titles.append(_entry[0])
+        except:
+            continue
+    xs = list(range(len(ys)))
+    high = titles[ys.index(max(ys))]
+    low = titles[ys.index(min(ys))]
+    print(high)
+    print(low)
+    plt.scatter(xs, ys)
+    plt.title(f"Obama Sentiments, {year}")
+    plt.xlabel("Headline Number")
+    plt.ylabel("Average Polarity")
+    plt.show()
+    
 # plot each year
+for year in range(2008, 2018):
+    plot_polarities(year)
